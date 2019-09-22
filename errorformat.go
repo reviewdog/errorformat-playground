ad29a7eaa6628a57b7e57799cdeec6efebd769ef
haya14busa <haya14busa@gmail.com>
2019-09-22 14:28:19 +0000

    init

---
+++ errorformat.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"strings"
+
+	"github.com/gopherjs/gopherjs/js"
+	"github.com/reviewdog/errorformat"
+)
+
+func main() {
+	js.Global.Set("errorformat", map[string]interface{}{"Errorformat": Errorformat})
+}
+
+// Errorformat returns JSON array of errorformat.Enttry in string or return
+// error.
+func Errorformat(efms []string, text string) (string, string) {
+	fmt, err := errorformat.NewErrorformat(efms)
+	if err != nil {
+		return "", err.Error()
+	}
+	var entries []*errorformat.Entry
+	s := fmt.NewScanner(strings.NewReader(text))
+	for s.Scan() {
+		entries = append(entries, s.Entry())
+	}
+	bs, err := json.Marshal(entries)
+	if err != nil {
+		return "", err.Error()
+	}
+	return string(bs), ""
+}
